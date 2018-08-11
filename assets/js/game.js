2f229e578366ec3d04af519507b39b9d540fcf18
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-11 16:07:09 -0700

    basic logic to make plays on the game board, increment turns, reset the state of the game

--- assets/js/game.js
+++ assets/js/game.js
@@ -3,12 +3,80 @@
 // IMPORTANT:  due to how the game works, I'm going by column then row.
 // think of the 2d array below as the game board rotated 90 degrees clockwise
 // the left side is the bottom of the board, the right side is the top
-const gameBoard = [
-    [0,0,0,0,0,0], // column 0
-    [0,0,0,0,0,0], // column 1
-    [0,0,0,0,0,0], // column 2
-    [0,0,0,0,0,0], // column 3
-    [0,0,0,0,0,0], // column 4
-    [0,0,0,0,0,0], // column 5
-    [0,0,0,0,0,0]  // column 6
-];
+const gameBoard = {
+    board: [
+        [0,0,0,0,0,0], // column 0
+        [0,0,0,0,0,0], // column 1
+        [0,0,0,0,0,0], // column 2
+        [0,0,0,0,0,0], // column 3
+        [0,0,0,0,0,0], // column 4
+        [0,0,0,0,0,0], // column 5
+        [0,0,0,0,0,0]  // column 6
+    ],
+    reset: function() {
+        for (let i = 0; i < this.board.length; i++) {
+            for (let j = 0; j < this.board[i].length; j++) {
+                this.board[i][j] = 0;
+            }
+        }
+    },
+    play: function(column) {
+        if (turnObj.turn !== 0) {
+            if (this.board[column].indexOf(0) !== -1) {
+                this.board[column][this.board[column].indexOf(0)] = turnObj.turn;
+                turnObj.play();
+            } else {
+                console.log("error:  column is full");
+            }
+            
+        } else {
+            console.log("error:  developer C is a nooblord and forgot to start the game");
+        }
+    }
+};
+
+const turnObj = {
+    turn: 0,
+    inverseTurn: 0,
+    turns: 0,
+    startGame: function() {
+        this.turn = 1;
+        this.inverseTurn = 2;
+    },
+    play: function() {
+        if (this.turn === 1) {
+            this.turn++;
+            this.inverseTurn--;
+        } else if (this.turn === 2) {
+            this.turn--;
+            this.inverseTurn++;
+        }
+
+        this.turns++;
+    },
+    reset: function() {
+        this.turn = 0;
+        this.inverseTurn = 0;
+        this.turns = 0;
+    }
+}
+
+const playColumn = function(columnId, turn) {
+
+}
+
+// ========== attach event handlers ==========
+
+const gameColumns = document.getElementsByClassName("game-column");
+
+for (let i = 0; i < gameColumns.length; i++) {
+    gameColumns[i].addEventListener("click", function(event) {
+        if (event.target.classList.contains("circle-cell")) {
+            console.log(event.target.parentElement.parentElement.id);
+        } else if (event.target.classList.contains("game-cell")) {
+            console.log(event.target.parentElement.id);
+        } else if (event.target.classList.contains("game-column")) {
+            console.log(event.target.id);
+        }
+    });
+}
