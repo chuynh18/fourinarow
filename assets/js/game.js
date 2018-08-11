1dbf3d5e6f1670b340f5bb60db6d05601615048c
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-11 15:19:42 -0700

    added some more classes and ids to the html

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,2 +1,14 @@
 "use strict";
 
+// IMPORTANT:  due to how the game works, I'm going by column then row.
+// think of the 2d array below as the game board rotated 90 degrees clockwise
+// the left side is the bottom of the board, the right side is the top
+const gameBoard = [
+    [0,0,0,0,0,0], // column 0
+    [0,0,0,0,0,0], // column 1
+    [0,0,0,0,0,0], // column 2
+    [0,0,0,0,0,0], // column 3
+    [0,0,0,0,0,0], // column 4
+    [0,0,0,0,0,0], // column 5
+    [0,0,0,0,0,0]  // column 6
+];
