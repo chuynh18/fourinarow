57cfe52f892cc146f8d7c16efbe6020aaad481d1
5820K <38088640+chuynh18@users.noreply.github.com>
2018-08-11 17:51:21 -0700

    win checking logic should work

--- assets/js/game.js
+++ assets/js/game.js
@@ -32,6 +32,139 @@ const gameBoard = {
         } else {
             console.log("error:  developer C is a nooblord and forgot to start the game");
         }
+    },
+    checkCol: function() {
+        for (let i = 0; i <= 6; i++) {
+            for (let j = 0; j < 2; j++) {
+                const array = [gameBoard.board[i][j], gameBoard.board[i][j+1], gameBoard.board[i][j+2], gameBoard.board[i][j+3]];
+
+                if (array.indexOf(0) === -1 && array.indexOf(1) === -1) {
+                    turnObj.winner = 2;
+
+                    gameBoard.highlightWin(i, j, "p2Win");
+                    gameBoard.highlightWin(i, j+1, "p2Win");
+                    gameBoard.highlightWin(i, j+2, "p2Win");
+                    gameBoard.highlightWin(i, j+3, "p2Win");
+
+                    console.log("col win p2");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                } else if (array.indexOf(0) === -1 && array.indexOf(2) === -1) {
+                    turnObj.winner = 1;
+
+                    gameBoard.highlightWin(i, j, "p1Win");
+                    gameBoard.highlightWin(i, j+1, "p1Win");
+                    gameBoard.highlightWin(i, j+2, "p1Win");
+                    gameBoard.highlightWin(i, j+3, "p1Win");
+
+                    console.log("col win p1");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                }
+            }
+        }
+    },
+    checkRow: function() {
+        for (let i = 0; i <= 3; i++) {
+            for (let j = 0; j < 6; j++) {
+                const array = [gameBoard.board[i][j], gameBoard.board[i+1][j], gameBoard.board[i+2][j], gameBoard.board[i+3][j]];
+
+                if (array.indexOf(0) === -1 && array.indexOf(1) === -1) {
+                    turnObj.winner = 2;
+
+                    gameBoard.highlightWin(i, j, "p2Win");
+                    gameBoard.highlightWin(i+1, j, "p2Win");
+                    gameBoard.highlightWin(i+2, j, "p2Win");
+                    gameBoard.highlightWin(i+3, j, "p2Win");
+
+                    console.log("row win p2");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                } else if (array.indexOf(0) === -1 && array.indexOf(2) === -1) {
+                    turnObj.winner = 1;
+
+                    gameBoard.highlightWin(i, j, "p1Win");
+                    gameBoard.highlightWin(i+1, j, "p1Win");
+                    gameBoard.highlightWin(i+2, j, "p1Win");
+                    gameBoard.highlightWin(i+3, j, "p1Win");
+
+                    console.log("row win p1");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                }
+            }
+        }
+    },
+    checkDiagA: function() {
+        for (let i = 0; i <= 3; i++) {
+            for (let j = 3; j <= 5; j++) {
+                const array = [gameBoard.board[i][j], gameBoard.board[i+1][j-1], gameBoard.board[i+2][j-2], gameBoard.board[i+3][j-3]];
+                
+                if (array.indexOf(0) === -1 && array.indexOf(1) === -1) {
+                    turnObj.winner = 2;
+
+                    gameBoard.highlightWin(i, j, "p2Win");
+                    gameBoard.highlightWin(i+1, j-1, "p2Win");
+                    gameBoard.highlightWin(i+2, j-2, "p2Win");
+                    gameBoard.highlightWin(i+3, j-3, "p2Win");
+
+                    console.log("diagA win p2");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                } else if (array.indexOf(0) === -1 && array.indexOf(2) === -1) {
+                    turnObj.winner = 1;
+
+                    gameBoard.highlightWin(i, j, "p1Win");
+                    gameBoard.highlightWin(i+1, j-1, "p1Win");
+                    gameBoard.highlightWin(i+2, j-2, "p1Win");
+                    gameBoard.highlightWin(i+3, j-3, "p1Win");
+
+                    console.log("diagA win p1");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                }
+            }
+        }
+    },
+    checkDiagB: function() {
+        for (let i = 0; i <= 3; i++) {
+            for (let j = 0; j <= 2; j++) {
+                const array = [gameBoard.board[i][j], gameBoard.board[i+1][j+1], gameBoard.board[i+2][j+2], gameBoard.board[i+3][j+3]];
+                
+                if (array.indexOf(0) === -1 && array.indexOf(1) === -1) {
+                    turnObj.winner = 2;
+
+                    gameBoard.highlightWin(i, j, "p2Win");
+                    gameBoard.highlightWin(i+1, j+1, "p2Win");
+                    gameBoard.highlightWin(i+2, j+2, "p2Win");
+                    gameBoard.highlightWin(i+3, j+3, "p2Win");
+
+                    console.log("diagB win p2");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                } else if (array.indexOf(0) === -1 && array.indexOf(2) === -1) {
+                    turnObj.winner = 1;
+
+                    gameBoard.highlightWin(i, j, "p1Win");
+                    gameBoard.highlightWin(i+1, j+1, "p1Win");
+                    gameBoard.highlightWin(i+2, j+2, "p1Win");
+                    gameBoard.highlightWin(i+3, j+3, "p1Win");
+
+                    console.log("diagB win p1");
+                    console.log(array);
+                    console.log(`Starting cell: (${i}, ${j})`);
+                }
+            }
+        }
+    },
+    check: function() {
+        this.checkCol();
+        this.checkRow();
+        this.checkDiagA();
+        this.checkDiagB();
+    },
+    highlightWin: function(i, j, playerClass) {
+        document.getElementById(`${i}-${j}`).classList.add(playerClass);
     }
 };
 
@@ -87,20 +220,25 @@ const gameColumns = document.getElementsByClassName("game-column");
 
 for (let i = 0; i < gameColumns.length; i++) {
     gameColumns[i].addEventListener("click", function(event) {
-        if (turnObj.turn !== 0) {
+        if (turnObj.turn && !turnObj.winner) {
             if (event.target.classList.contains("circle-cell")) {
-                console.log(event.target.parentElement.parentElement.id);
+                // console.log(event.target.parentElement.parentElement.id);
                 gameBoard.play(event.target.parentElement.parentElement.id);
                 renderGame();
+                gameBoard.check();
             } else if (event.target.classList.contains("game-cell")) {
-                console.log(event.target.parentElement.id);
+                // console.log(event.target.parentElement.id);
                 gameBoard.play(event.target.parentElement.id);
                 renderGame();
+                gameBoard.check();
             } else if (event.target.classList.contains("game-column")) {
-                console.log(event.target.id);
+                // console.log(event.target.id);
                 gameBoard.play(event.target.id);
                 renderGame();
+                gameBoard.check();
             }
+        } else if (turnObj.winner) {
+            console.log(`Player ${turnObj.winner} won already.`);
         } else {
             console.log("error:  developer C is a nooblord and forgot to start the game");
         }
